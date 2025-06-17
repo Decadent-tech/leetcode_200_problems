b415fb08c0350bf409d589b13e4354fe132d89d8
Decadent-tech <debosmitac8@gmail.com>
2025-06-17 10:21:23 +0530

    Initial commit

--- py3.py
+++ py3.py
@@ -26,3 +26,73 @@ def maxSubArray(nums):
     return ans
 
 print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+
+####################################################################################
+x = 24 
+# Check the remainder dividing x by 2 is 0
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Checking another number    
+x = 7
+
+if x % 2 == 0:
+
+  print("Even")
+else:
+    print("Odd")
+#########################################
+a=[1, 2, 3, 4, 5]
+res = map(lambda num:str(num)+"Even" if num%2 ==0 
+          else str(num)+"Odd", a)
+print(list(res))  # ['1Odd', '2Even', '3Odd', '4Even', '5Odd']
+
+#############################################
+# Inputing Natural Number
+number = int(input("Enter the Natural Number: "))
+
+# j ranges from 1 to n
+for j in range(1, number+1):
+
+    # Initializing List
+    a = []
+
+    # i loop ranges from 1 to j
+    for i in range(1, j+1):
+        print(i, sep=" ", end=" ")
+        if(i < j):
+            print("+", sep=" ", end=" ")
+        a.append(i)
+    print("=", sum(a))
+
+print()
+
+#####################################################
+n= int(input("Enter a number to check if it is prime: "))
+import math
+is_prime = True
+for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            is_prime = False
+            break
+print(is_prime)
+
+###########################################
+
+# Function to print full pyramid pattern
+def full_pyramid(n):
+    for i in range(1, n + 1):
+        # Print leading spaces
+        for j in range(n - i):
+            print(" ", end="")
+        
+        # Print asterisks for the current row
+        for k in range(1, 2*i):
+            print("*", end="")
+        print()
+   
+full_pyramid(5)
+
+##############################################
